74f8b7cb90435948d96863739313afe0a47a66ee
Anton S <geodesist.sushkov@gmail.com>
2021-12-22 23:43:42 +0300

    Добавьте файлы проекта.

---
+++ Task15/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression ar = new ArithProgression();
+            Console.WriteLine("установите начальное значение");
+            int x = Convert.ToInt32(Console.ReadLine());
+            ar.setStart(x);
+            Console.WriteLine("установите шаг");
+            int s = Convert.ToInt32(Console.ReadLine());
+            ar.setStep(s);
+            Console.WriteLine("арифм. прогрессия:");
+            Console.WriteLine(ar.getNext());
+            Console.WriteLine(ar.getNext());
+            Console.WriteLine(ar.getNext());
+            Console.WriteLine("сброс");
+            ar.reset();
+            Console.WriteLine(ar.getNext());
+
+            GeomProgression geom = new GeomProgression();
+            Console.WriteLine("\nгеом. прогрессия:");
+            geom.setStart(x);
+            geom.setStep(s);
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine("сброс");
+            geom.reset();
+            Console.WriteLine(geom.getNext());
+            Console.ReadKey();
+        }
+    }
+    interface ISeries
+    {
+        void setStart(int x);
+        int getNext();
+        void reset();
+    }
+    class ArithProgression : ISeries
+    {
+        int current;
+        int step;
+        int first;
+
+        public void setStart(int x)
+        {  
+            first = x;
+            current = first;
+        }
+
+        public int getNext()
+        {    
+            current += step;
+            return current;
+        }
+
+
+        public void setStep(int s)
+        {
+            step = s;
+        }
+
+        public void reset()
+        {
+            current = first;
+        }
+    }
+    class GeomProgression : ISeries
+    {
+        int current;
+        int step;
+        int first;
+
+        public void setStart(int x)
+        {
+            first = x;
+            current = first;
+        }
+
+        public int getNext()
+        {
+            current *= step;
+            return current;
+        }
+
+
+        public void setStep(int s)
+        {
+            step = s;
+        }
+
+        public void reset()
+        {
+            current = first;
+        }
+    }
+}
